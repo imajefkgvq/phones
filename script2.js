264ff2b1ba1c48c93f28f665892b0d67f2c50249
Zhaksylyk Imangali <user@MacBook-Air--imangali.local>
2025-02-24 15:37:08 +0500

    Another update

--- script2.js
+++ script2.js
@@ -1,19 +1,22 @@
 function toggleMenu() {
   document.getElementById("head3").classList.toggle("active");
 }
-
-async function show() {
+async function showPhone() {
   const api = "https://twof2-iphone.onrender.com/iphones";
   const respo = await fetch(api);
-  const data = await respo.json();
+  let data = await respo.json();
+  data = data.slice(12, 22);
+
   console.log(data);
+
   const namePhone = document.querySelectorAll("#name");
   const description = document.querySelectorAll("#description");
   const cost = document.querySelectorAll("#cost");
-  for (i = 0; i <= 11; i++) {
+
+  for (let i = 0; i < data.length; i++) {
     namePhone[i].textContent = data[i].model;
     description[i].textContent = data[i].description;
     cost[i].textContent = data[i].price;
   }
 }
-show();
+showPhone();
